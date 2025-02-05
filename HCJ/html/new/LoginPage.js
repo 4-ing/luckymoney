c1a9f4e868f19eb5f1dabb8a55da7f5d410a2cac
kimhyunggue <hyo06069@naver.com>
2025-02-05 14:35:21 +0900

    Feature : 복주머니 로그인 페이지

---
+++ HCJ/html/new/LoginPage.js
@@ -0,0 +1,14 @@
+function loginWithGoogle() {
+    alert("구글 로그인 페이지로 이동합니다! (API 연동 필요)");
+    // 실제로는 OAuth URL로 이동해야 합니다.
+}
+
+function loginWithKakao() {
+    alert("카카오 로그인 페이지로 이동합니다! (API 연동 필요)");
+    // 실제로는 Kakao 로그인 URL로 이동해야 합니다.
+}
+
+function loginWithNaver() {
+    alert("네이버 로그인 페이지로 이동합니다! (API 연동 필요)");
+    // 실제로는 Naver OAuth URL로 이동해야 합니다.
+}

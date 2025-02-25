aab978741364cb2514ebd90800e7eb908af80a64
yoon-0409 <quddbs0409@gmail.com>
2025-02-25 21:43:25 +0900

    초기화

--- HCJ/html/another/writingLucky.js
+++
@@ -1,6 +0,0 @@
-const luckeyLetterInput = document.getElementById('luckeyLetterInput');
-const nextBtn = document.getElementById('nextBtn');
-nextBtn.addEventListener('click', function() {
-    const textareaContent = luckeyLetterInput.value;
-    console.log(textareaContent);
-});
